06a3c32b2a68e29446ac561ff15e8135a4d1b164
Yosef64 <98908763+Yosef64@users.noreply.github.com>
2024-02-01 23:13:00 +0300

    Create mod_pow.py

---
+++ mod_pow.py
@@ -0,0 +1,6 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+first  = int(input())
+second = int(input())
+third = int(input())
+print(pow(first,second))
+print(pow(first,second,third))
